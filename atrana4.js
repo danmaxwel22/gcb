526f27757529d3ba1151ca4857c8f71b1d5cc0d1
I'm Dan <73338778+danmaxwel22@users.noreply.github.com>
2023-12-01 15:50:39 +0800

    Update atrana4.js

--- atrana4.js
+++ atrana4.js
@@ -1,7 +1,7 @@
 var scriptElement = document.createElement('script');
 
 // Set the source attribute to the local jQuery file
-scriptElement.src = 'path/to/jquery-3.6.4.min.js';
+scriptElement.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js';
 
 // Append the script element to the head tag
 document.head.appendChild(scriptElement);
