b7beb89cfb3b58e4bb2375fc4795958b214ffc23
I'm Dan <73338778+danmaxwel22@users.noreply.github.com>
2024-02-12 15:38:16 +0800

    updates2

--- report.js
+++ report.js
@@ -8,7 +8,7 @@ function loadScript(src, callback) {
 // List of library URLs
 var libraryUrls = [
     'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js',
-    'ttps://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js'
     // Add more library URLs as needed
 ];
@@ -20,6 +20,7 @@ function loadLibraries(index) {
             // Continue loading the next library
             loadLibraries(index + 1);
         });
+        console.log("msg")
     } else {
        $(function() {
 
